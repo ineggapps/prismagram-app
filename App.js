0d6ee84e16a7d96d1f906cce08f6833f6fe453f6
ineggapps <inegg.apps@gmail.com>
2019-07-30 23:01:16 +0900

    Setting for using apollo client

--- App.js
+++ App.js
@@ -4,16 +4,35 @@ import { AppLoading } from "expo";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
 import { Text, View } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo-hooks";
+import apolloClientOptions from "./apollo";
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
   const preLoad = async () => {
     try {
       await Font.loadAsync({
         ...Ionicons.font
       });
       await Asset.loadAsync([require("./assets/logo.png"), require("./assets/icon.png")]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage
+      });
+
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions
+      });
+
       setLoaded(true);
+      setClient(client);
     } catch (e) {
       console.log(e);
     }
@@ -21,11 +40,16 @@ export default function App() {
   useEffect(() => {
     preLoad();
   }, []);
-  return loaded ? (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </ApolloProvider>
   ) : (
-    <AppLoading />
+    <>
+      로드중입니다.
+      <AppLoading />
+    </>
   );
 }
---
+++ apollo.js
@@ -0,0 +1,5 @@
+const options = {
+  uri: "http://10.0.0.10:4000/graphql"
+};
+
+export default options;
